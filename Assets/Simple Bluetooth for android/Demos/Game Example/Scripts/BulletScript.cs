c39f97fc8c6fc1659d218921751c064b90195e0c
tetoravr <tetosants@tetoravr.com>
2021-03-03 13:21:51 +0900

    Simple Bluetooh for android

---
+++ Assets/Simple Bluetooth for android/Demos/Game Example/Scripts/BulletScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    // collision check. If the projectile collides with a wall or with its own tank, then it is simply destroyed.
+    // If the projectile collides with an enemy tank, then it is destroyed and calls the method of reducing HP in the GameScene script.
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "LeftBorder" || collision.gameObject.name == "RightBorder" || collision.gameObject.name == "PlayerTank") {
+            Destroy(gameObject);
+        }
+        else {
+            GameScene.instance.HitByEnemy();
+            Destroy(gameObject);
+        }
+    }
+}
